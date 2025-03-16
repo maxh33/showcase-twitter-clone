076454f7ac64ab46e41a2809acb4ebdfc6cc2904
maxh33 <contact@maxhaider.dev>
2025-03-16 07:23:17 -0300

    Fix: Improve TypeScript module handling in build process

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -18,5 +18,6 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-// Added to satisfy TypeScript's --isolatedModules flag
+// This empty export is required to make TypeScript treat this file as a module
+// when using the isolatedModules compiler flag (default in Create React App)
 export {};
