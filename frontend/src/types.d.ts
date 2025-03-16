9c0525c10aac60e7025e913e82e0e0b6c558933b
maxh33 <contact@maxhaider.dev>
2025-03-16 07:25:38 -0300

    Fix: Add TypeScript declarations file to improve module resolution

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,9 @@
+// Global type declarations
+declare module '*.jpg';
+declare module '*.png';
+declare module '*.svg' {
+  import React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+} 
